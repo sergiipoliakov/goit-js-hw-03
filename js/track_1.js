60d0d28af7c32b4875c534ce9e512171f29a174a
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-08-30 14:10:06 +0200

    Update track_1.js

--- js/track_1.js
+++ js/track_1.js
@@ -6,15 +6,6 @@
 // заменяет значение premium на false
 // выводит содержимое объекта user в формате ключ: значение используя Object.keys() и for...of
 
-// const user = {
-//   name: 'Mango',
-//   age: 20,
-//   hobby: 'html',
-//   premium: true,
-// };
-// user.mood = 'happy';
-// console.log(user);
-
 let message = '';
 const user = {
   age: 20,
@@ -23,25 +14,15 @@ const user = {
   premium: true,
 };
 
-//Write code under this line
-
 user.mood = 'happy';
 user.hobby = 'skydiving';
 user.premium = false;
 user['full time'] = true;
 
 const keys = Object.keys(user);
-// Write code under this line
+
 for (const key of keys) {
   message += `${key} : ${user[key]}\n`;
 }
 
 console.log(message);
-/*
-'age : 20
-hobby : skydiving
-name : Mango
-premium : false
-mood : happy
-"full time" : true
-' */
